f33b1bdf5347d3a740ac8c0df99e58c07b79791f
deionstfleur <67560524+deionstfleur@users.noreply.github.com>
2020-11-17 10:40:11 -0500

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,17 +1,27 @@
 <?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+  $email_from = 'saintfamilvisuals@gmail.com';
+  
+  $email_subject = "New Form Submission";
 
-    $mailTo = "legacyglooringllc20@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name;".\n\n".$message;
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
 
-    
-    mail($mailTo, $subject, $txt, $headers);
 
-}
+  $to = "deionstfleur13@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("Location: index.html");
+
+
+?>
 
